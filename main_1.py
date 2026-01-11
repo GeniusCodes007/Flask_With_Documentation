96d373c04119be469eefb3828ac5e0fb6d72abda
GeniusCodes007 <geniuscodes007@gmail.com>
2026-01-11 18:25:44 +0100

    Lesson Continues

--- main_1.py
+++ main_1.py
@@ -1,6 +1,21 @@
 from flask import Flask
 from markupsafe import escape
 
+"""
+To run a Flask file, there are two options:
+Let's say we have a Python file, 'main.py', and in this file we have a Flask object app, i.e. app = Flask(__name__).
+Then to run this Python Flask file:
+Method 1
+-> flask --app main_1 run --debug
+
+Method 2
+-> flask --app main_1 run
+
+If I had added the line code 
+app.run(debug=True)
+Then we would have to run 'python file_name.py' in the terminal 
+"""
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +26,7 @@ def hello_world():
 def name(my_name:str):
     return f"Hello, {escape(my_name)}"
 
-@app.route('/user/<int : id_>')
+@app.route('/user/id_')
 def name2(id_):
     try:
         return f"Welcome, {id_}"
